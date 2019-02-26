7040c054ec0f48ebce8f8fd5b1e76296ea02e3eb
Gruiz016 <meet.gio@icloud.com>
2019-02-26 18:14:51 -0500

    first commit for javascript basics

---
+++ basics/numbers.js
@@ -0,0 +1,23 @@
+/* LESSION BELOW */
+
+// let num = 5.5 * 2
+// let num = 5.5 - 1
+// let num = 5.5 + 3
+
+//let x = 11
+/* Order of operations is * > / > + > - */
+//let num = x + 1 * 2
+
+/*DOG YEARS EQUATIONS*/
+//let age = 29
+//let dogYears = (age + 1) / 7
+
+//console.log(dogYears)
+
+/*CHALLENGE BELOW*/
+
+let studentScore = 9
+let maxScore = 20
+let percentScore = (studentScore / maxScore) * 100
+
+console.log(percentScore)
---
+++ basics/strings.js
@@ -0,0 +1,15 @@
+/* BELOW IS THE STRINGS LESSION
+let firstName = 'Giovanni'
+let lastName = 'Ruiz'
+let fullName = firstName + ' ' + lastName
+
+console.log(fullName)
+*/
+
+/* BELOW IS THE STRINGS CHALLENGE - Create a variable for City, Country, and output it as a location */
+
+let city = 'Connecticut'
+let country = 'USA'
+let location = city + ', ' + country
+
+console.log(location)
