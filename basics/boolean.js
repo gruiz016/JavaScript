ada96efb226a5fb510cafc3459daf14221eb9f41
Gruiz016 <meet.gio@icloud.com>
2019-02-26 20:50:00 -0500

    updating boolean.js

--- basics/boolean.js
+++ basics/boolean.js
@@ -1,11 +1,25 @@
-let temp = 31
+//let temp = 31
 // === is the equality operator 
 //let isFreezing = temp === 32
 
 //!== is the NOT equal operator
 //let isFreezing = temp !== 32
 
-let isFreezing = temp 
+// < (less than) operator
+//let isFreezing = temp < 32
 
+// > (greater than) operator
+//let isFreezing = temp > 32
 
-console.log(isFreezing)
+//<=(less than or equal to) operator
+//>= (greater than or equal to) operator
+
+//console.log(isFreezing)
+
+/*CHALLEGE create a age determinator to see if you can offer discounts based on age*/
+let age = 29
+let child = age <= 7
+let senoir = age >= 65
+
+console.log(child)
+console.log(senoir)
