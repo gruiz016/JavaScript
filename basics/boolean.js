003cc1150f4e12e901700655f1588662fa86c18c
Gruiz016 <meet.gio@icloud.com>
2019-02-26 20:36:02 -0500

    adding a boolean.js file

---
+++ basics/boolean.js
@@ -0,0 +1,11 @@
+let temp = 31
+// === is the equality operator 
+//let isFreezing = temp === 32
+
+//!== is the NOT equal operator
+//let isFreezing = temp !== 32
+
+let isFreezing = temp 
+
+
+console.log(isFreezing)
