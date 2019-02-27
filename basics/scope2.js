54fce9b717d6a9bcfcd99397842a0f7002e980a2
Gruiz016 <meet.gio@icloud.com>
2019-02-27 18:04:31 -0500

    Finishing up the basics with scope lessons

---
+++ basics/scope2.js
@@ -0,0 +1,19 @@
+//Global (name)
+  //local (name)
+    //local
+  //local
+
+//let name = 'Giovanni'
+
+if (true) {
+  //let name = 'Mike'
+
+  if (true) {
+    name = 'Jen'
+    console.log(name)
+  }
+}
+
+if (true) {
+  console.log(name)
+}
