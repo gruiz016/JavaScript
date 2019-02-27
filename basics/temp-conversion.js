79452043f0bfb8efb7b8d924aafcb6ff762e92a7
Gruiz016 <meet.gio@icloud.com>
2019-02-26 19:04:12 -0500

    adding a few more files

---
+++ basics/temp-conversion.js
@@ -0,0 +1,7 @@
+let fahrenheit = 32
+let celsius = (fahrenheit - 32) * 5 / 9
+console.log('Fahrenheit converted is ' + celsius + ' in Celsius.')
+
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log('Fahrenheit converted is ' + kelvin + ' in Kelvin.')
---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+// RULE 1. YOu can not define a variable more then once!
+let petName = 'Nala'
+petName = 'Spot'
+
+console.log(petName)
+
+// RULE 2. There are rules related to variable names
+
+//let 3 = 1
+//let !nice = 'not a good variable name'
+
+// RULE 3. Variables can not be reserved keywords
+
+//let let = 'Giovanni'
